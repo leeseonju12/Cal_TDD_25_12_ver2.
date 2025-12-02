555e38b7f86b8531a5f98df9ca4457b2fe5c0f2b
leeseonju12 <seonju220@gmail.com>
2025-12-02 12:00:28 +0900

    git link test

---
+++ src/main/java/org/example/Calc.java
@@ -0,0 +1,47 @@
+package org.example;
+
+public class Calc {
+
+    public static int run(String exp) { // 10 + 5 * 2
+
+        boolean needToMulti = exp.contains("*");
+        boolean needToPlus = exp.contains("+");
+
+        boolean needToCompound = needToPlus && needToMulti;
+
+        exp = exp.replace("- ", "+ -");
+
+        if (needToCompound) {
+            String[] bits = exp.split(" \\+ ");
+
+            return Integer.parseInt(bits[0]) + run(bits[1]);
+        }
+
+        if (needToPlus) {
+            String[] bits = exp.split(" \\+ ");
+            int sum = 0;
+
+            for (int i = 0; i < bits.length; i++) {
+                sum += Integer.parseInt(bits[i]);
+            }
+
+            return sum;
+
+        } else if (needToMulti) {
+            String[] bits = exp.split(" \\* ");
+
+            int sum = 1;
+
+            for (int i = 0; i < bits.length; i++) {
+                sum *= Integer.parseInt(bits[i]);
+            }
+
+            return sum;
+        }
+
+
+        throw new RuntimeException("해석 불가 : 올바른 계산식이 아님");
+    }
+
+
+}
